cdc2e744ae20f04a3d3bcde84ae6c8621e35bea2
Danielxxf <374526876@qq.com>
2023-07-09 16:22:03 +0800

    fix and optimize

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,16 @@
 import * as vscode from 'vscode';
 import { Configuration, OpenAIApi } from "openai";
 import * as fs from 'fs';
+import * as path from 'path';
 
+
+// 激活函数
 export function activate(context: vscode.ExtensionContext) {
-  console.log('Congratulations, your extension "auto-comment" is now active!');
+  console.log('Congratulations, your extension "auto-comment" is now active!'); // 输出激活成功的消息
 
-  const configPath = './config.json'; // 配置文件路径
-  let apiKey:string = '';
-  let model:string = '';
+  const configPath = path.resolve(__dirname, '../config/config.json'); // 定义配置文件路径
+  let apiKey: string; // 定义apiKey变量并赋值
+  let model: string; // 定义model变量并赋值
 
   try {
     const data = fs.readFileSync(configPath, 'utf-8');
@@ -18,6 +21,7 @@ export function activate(context: vscode.ExtensionContext) {
     console.log(`Failed to read config file: ${err}`);
     return;
   }
+  console.log(apiKey);
   
   // 创建一个新的Configuration对象，将API密钥传递给它进行配置
   const configuration = new Configuration({
@@ -27,39 +31,39 @@ export function activate(context: vscode.ExtensionContext) {
   // 用上面创建的Configuration对象来实例化一个新的OpenAIApi对象，并传递密钥进行身份验证
   const openai = new OpenAIApi(configuration);
 
-  let disposable = vscode.commands.registerCommand('commentgpt.generateComment', () => {
+  let disposable = vscode.commands.registerCommand('commentgpt.generateComment', async () => {
     const editor = vscode.window.activeTextEditor;
     if (editor) {
       const document = editor.document;
       const selection = editor.selection;
       const codeSnippet = document.getText(selection);
 
-      // 使用 ChatGPT API 生成注释文本
-      generateComment(codeSnippet, openai)
-        .then((comment) => {
-          console.log(comment);
-          if (comment && comment.length > 0) {
-            editor.edit((editBuilder) => {
-              editBuilder.insert(selection.start, `/*`);
-              editBuilder.insert(selection.end, `*/\n`);
-              editBuilder.insert(selection.end, `\n${comment}\n`);
-            });
-          }
-        })
-        .catch((error) => {
-          vscode.window.showErrorMessage('Failed to generate comment: ' + error.message);
-        });
+      try {
+        // 使用 ChatGPT API 生成注释文本
+        const comment = await generateComment(codeSnippet, openai, model);
+        console.log(comment);
+        if (comment && comment.length > 0) {
+          editor.edit((editBuilder) => {
+            editBuilder.insert(selection.start, `/*`);
+            editBuilder.insert(selection.end, `*/\n`);
+            editBuilder.insert(selection.end, `\n${comment}\n`);
+          });
+        }
+      } catch (error) {
+        vscode.window.showErrorMessage('Failed to generate comment: ' + error);
+      }
     }
   });
 
   context.subscriptions.push(disposable);
 }
 
-async function generateComment(codeSnippet: string, openai: OpenAIApi): Promise<string> {
+async function generateComment(codeSnippet: string, openai: OpenAIApi, model: string): Promise<string> {
   return new Promise<string>((resolve, reject) => {
+    console.log(codeSnippet);
     openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: '请为以下代码添加详尽的注释，如果没给出代码，你就报错\n' + codeSnippet }],
+      model: model,
+      messages: [{ role: 'user', content: '请为以下代码添加详尽的注释，不要修改源代码，如果没给出代码，请报错\n' + codeSnippet }],
     })
       .then((response) => {
         const message = response.data.choices[0]?.message?.content;
